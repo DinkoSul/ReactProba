6e1973577e04a76cc38ead5f5c007ed7d22f1768
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-10-25 21:07:29 +0200

    ReactPočetno

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
+import randomstring from "randomstring";
+import person from "./Person";
+import { num1, num2 } from "./Utility";
+// import { sum as zbroj } from "./Utility";
+import * as Math from "./Utility";
+// import Komponenta1 from "./Components/Komponenta1";
+// import Komponenta2 from "./Components/Komponenta2";
+// import Komponenta3 from "./Components/Komponenta3";
+import { Komponenta1, Komponenta2, Komponenta3 } from "./Components";
 
 function App() {
+  var x = 2;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Komponenta1 />
+      <Komponenta2 />
+      <Komponenta3 />
+      <h1>Bok {person.name}</h1>
+      <p>Prvi broj: {num1 + x}</p>
+      <p>Drugi broj: {num2}</p>
+      {/* <p>
+        Zbroj {num1} i {num2} je {zbroj(num1, num2)}
+      </p> */}
+      <hr></hr>
+      <p>Zbroj brojeva je: {Math.sum(Math.num1, Math.num2)}</p>
+      <hr></hr>
+      <p>
+        Random riječi:
+        {randomstring.generate({
+          length: 12,
+          charset: "alphabetic",
+        })}
+      </p>
     </div>
   );
 }
 
 export default App;
+
+// 1.	Napraviti novu React aplikaciju
+// 2.	Unutar App komponente napraviti dvije varijable koje sadrže neki string
+// 3.	Napraviti JSX element koji će prikazati neki tekst na ekranu
+// 4.	Napraviti JSX element koji će prikazati neki tekst na ekranu + vrijednost jedne od varijabli
+// 5.	Napraviti JSX element koji će prikazati obje varijable odvojene razmakom
